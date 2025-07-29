c690d29bd061353f70f36b83422c649b1879e31c
Prashant <53570660+prashant3110@users.noreply.github.com>
2025-07-29 08:57:47 +0530

    Debug change

--- std_management/settings.py
+++ std_management/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=z0d57msmx+o3@#lgt^5(*b!*3vjtre=h+%#f)*4v@(!k0!rqr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
